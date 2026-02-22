acc0760e1492071ce3cb46253f1c6cde560c5870
Fririz <Fririzriz@gmail.com>
2026-02-22 20:39:34 +0200

    feat: add Swagger

--- src/Backend.Api/Program.cs
+++ src/Backend.Api/Program.cs
@@ -4,6 +4,7 @@ using Backend.Modules.SomeEntity;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddEntityModules(builder.Configuration);
 
 var app = builder.Build();
@@ -13,6 +14,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend.Api v1"));
 }
 
 app.UseHttpsRedirection();
