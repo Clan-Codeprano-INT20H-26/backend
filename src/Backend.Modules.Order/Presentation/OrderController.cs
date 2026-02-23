5aa5b63b1533079ed17a960de87c6cbb06113057
Fririz <Fririzriz@gmail.com>
2026-02-24 01:21:39 +0200

    feat: improve orders module

--- src/Backend.Modules.Order/Presentation/OrderController.cs
+++ src/Backend.Modules.Order/Presentation/OrderController.cs
@@ -1,25 +1,37 @@
 using Backend.Modules.Order.Application;
 using Backend.Modules.Shared.DTOs.Order;
+using Backend.Modules.Shared.Interfaces.Auth;
 using Backend.Modules.Shared.Interfaces.Order;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
 namespace Backend.Modules.Order.Presentation;
 
 [ApiController]
 [Route("api/[controller]")]
+[Produces("application/json")]
 public class OrderController : ControllerBase
 {
     private readonly IOrderService _orderService;
+    private readonly IUserService _userService;
 
-    public OrderController(IOrderService orderService)
+    public OrderController(IOrderService orderService, IUserService userService)
     {
         _orderService = orderService;
+        _userService = userService;
     }
+    [Authorize]
     [HttpPost]
-    public async Task<IActionResult> Create(OrderCreateDto dto)
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Create([FromBody] OrderCreateDto dto)
     {
-        var userId = Guid.NewGuid(); //GetFromJwt
+        var userId = _userService.GetUserIdFromJwt(User);
         
-        var result = await _orderService.CreateOrderAsync(dto, userId);
+
+        var result = await _orderService.CreateOrderAsync(dto, userId!.Value);
 
         if (result.IsFailed)
         {
@@ -28,17 +40,34 @@ public class OrderController : ControllerBase
 
         return Ok(result.Value);
     }
-
-    [HttpGet("{id}")]
+    [Authorize]
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get(Guid id)
     {
-        var result = await _orderService.GetByIdAsync(id);
+        var userId = _userService.GetUserIdFromJwt(User);
+        var result = await _orderService.GetByIdAsync(id, userId!.Value);
 
         if (result.IsFailed)
         {
-            return NotFound(result.Errors.First().Message);
+            return NotFound(new { message = result.Errors.First().Message });
         }
 
         return Ok(result.Value);
+    }
+    [Authorize]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAll()
+    {
+        var userId = _userService.GetUserIdFromJwt(User);
+        var result = await _orderService.GetAllAsync(userId!.Value);
+        if (result.IsFailed)
+        {
+            return NotFound(new { message = result.Errors.First().Message });
+        }
+        return  Ok(result.Value);
     }
 }
--- src/Backend.Modules.Shared/DTOs/Order/OrderCreateDto.cs
+++ src/Backend.Modules.Shared/DTOs/Order/OrderCreateDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.Modules.Shared.DTOs.Order;
 
 public class OrderCreateDto
 {
     public List<Guid> kitId { get; set; } = new();
+    [Required]
     public string? latitude { get; set; }
+    [Required]
     public string? longitude { get; set; }
 }
