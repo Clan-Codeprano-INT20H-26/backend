09ca309f025ae5f2ebcd1b5666eafb05254932f0
Fririz <Fririzriz@gmail.com>
2026-02-23 21:09:18 +0200

    Entity base updated

--- src/Backend.Modules.Shared/Models/EntityBase.cs
+++ src/Backend.Modules.Shared/Models/EntityBase.cs
@@ -2,6 +2,6 @@ namespace Backend.Modules.Shared.Models;
 
 public class EntityBase
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 }
