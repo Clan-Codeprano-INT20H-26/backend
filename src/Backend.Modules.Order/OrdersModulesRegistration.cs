558a60928cd89c1ce39e0d43c4b633c97073e80f
Fririz <Fririzriz@gmail.com>
2026-02-23 21:11:09 +0200

    feat: add order entity

---
+++ src/Backend.Modules.Order/OrdersModulesRegistration.cs
@@ -0,0 +1,23 @@
+using Backend.Modules.Order.Application;
+using Backend.Modules.Order.Application.Interfaces;
+using Backend.Modules.Order.Infrastructure;
+using Backend.Modules.Shared.Interfaces.Order;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backend.Modules.Order;
+
+public static class OrdersModulesRegistration
+{
+    public static IServiceCollection AddOrdersModules(this IServiceCollection services, IConfiguration manager)
+    {
+        services.AddScoped<ITaxHelper, TaxHelper>();
+        services.AddScoped<IOrderService, OrderService>();
+        services.AddDbContext<Infrastructure.OrderDbContext>(options =>
+            options.UseNpgsql(manager.GetConnectionString("Postgres")));
+        services.AddControllers()
+            .AddApplicationPart(typeof(Presentation.OrderController).Assembly); //add controllers
+        return services;
+    }
+}
