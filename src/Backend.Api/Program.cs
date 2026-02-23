ebdcd473090f7155bfd26ddff636e45d6d2f50bc
Fririz <Fririzriz@gmail.com>
2026-02-23 22:40:47 +0200

    fix

--- src/Backend.Api/Program.cs
+++ src/Backend.Api/Program.cs
@@ -1,3 +1,4 @@
+using Backend.Modules.Order;
 using Backend.Modules.SomeEntity;
 using Backend.Modules.SomeEntity.Infrastructure;
 using Microsoft.EntityFrameworkCore;
@@ -8,14 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEntityModules(builder.Configuration);
+builder.Services.AddOrdersModules(builder.Configuration);
 
-builder.Services.AddDbContext<SomeEntityDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-if (string.IsNullOrEmpty(builder.Configuration.GetConnectionString("DefaultConnection")))
-{
-    throw new InvalidOperationException("String connection 'DefaultConnection' is not found.");
-}
 
 builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
@@ -32,7 +28,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend.Api v1"));
 }
 
-app.UseHttpsRedirection();
-
+app.MapControllers();
 
 app.Run();
