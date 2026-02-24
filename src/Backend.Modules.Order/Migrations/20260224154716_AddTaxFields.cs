0a907f094a843aaf4bb0e41ceff9a9452c341dba
Fririz <Fririzriz@gmail.com>
2026-02-24 18:05:21 +0200

    fir: minor

---
+++ src/Backend.Modules.Order/Migrations/20260224154716_AddTaxFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Modules.Order.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaxFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CompositeTaxRate",
+                table: "Orders",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TaxAmount",
+                table: "Orders",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompositeTaxRate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TaxAmount",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "Orders");
+        }
+    }
+}
