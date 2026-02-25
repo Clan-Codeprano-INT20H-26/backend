98da4900fad8d5cac22fc355e3f9c64fbf888af0
Fririz <Fririzriz@gmail.com>
2026-02-25 10:47:20 +0200

    feat: images implemented

--- src/Backend.Module.Kit/Infrastructure/KitDbContext.cs
+++ src/Backend.Module.Kit/Infrastructure/KitDbContext.cs
@@ -1,10 +1,33 @@
+using Backend.Module.Kit.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Module.Kit.Infrastructure;
 
 public class KitDbContext : DbContext
 {
-    public KitDbContext(DbContextOptions<KitDbContext> options) : base(options) { }
+    public DbSet<Domain.Kit> Kits { get; set; }
 
-    public DbSet<Domain.Kit> Kits => Set<Domain.Kit>();
+    public KitDbContext(DbContextOptions<KitDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Domain.Kit>(entity =>
+        {
+            entity.HasKey(k => k.Id);
+
+            entity.Property(k => k.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(k => k.Price)
+                .HasPrecision(18, 2); 
+            
+            entity.Property(k => k.Images)
+                .HasColumnType("jsonb"); 
+        });
+    }
 }
---
+++ src/Backend.Modules.Shared/SharedModuleRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Modules.Shared.Infrastructure;
+using Backend.Modules.Shared.Interfaces.Image;
+using Backend.Modules.Shared.Interfaces.Kit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backend.Modules.Shared;
+
+public static class SharedModuleRegistration
+{
+    public static IServiceCollection AddSharedModule(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddScoped<IImageStorage, ImageStorage>();
+        return services;
+    }
+}
