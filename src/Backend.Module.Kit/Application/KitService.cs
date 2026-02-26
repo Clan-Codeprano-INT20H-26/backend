2c623b912b6561a4d4bfac67764446419f5fd099
Fririz <fririzriz@gmail.com>
2026-02-26 15:03:02 +0200

    add trigram search

--- src/Backend.Module.Kit/Application/KitService.cs
+++ src/Backend.Module.Kit/Application/KitService.cs
@@ -31,8 +31,9 @@ public class KitService : IKitService
             if (!string.IsNullOrWhiteSpace(filter.SearchTerm))
             {
                 var term = filter.SearchTerm.ToLower();
-                query = query.Where(k => k.Name.ToLower().Contains(term) 
-                                      || k.Description.ToLower().Contains(term));
+                query = query.Where(k => 
+                    EF.Functions.TrigramsSimilarity(k.Name, term) > 0.2 || 
+                    EF.Functions.TrigramsSimilarity(k.Description, term) > 0.2);
             }
 
             if (!string.IsNullOrWhiteSpace(filter.Seller))
@@ -53,6 +54,25 @@ public class KitService : IKitService
             
             var sortBy = filter.SortBy?.ToLower()?.Trim();
             var isDesc = filter.IsDescending;
+            if (!string.IsNullOrWhiteSpace(filter.SearchTerm) && sortBy == "relevance")
+            {
+                var term = filter.SearchTerm.Trim();
+                query = query.OrderByDescending(k => 
+                    Math.Max(
+                        EF.Functions.TrigramsSimilarity(k.Name, term), 
+                        EF.Functions.TrigramsSimilarity(k.Description, term)
+                    ));
+            }
+            else
+            {
+                query = sortBy switch
+                {
+                    "price" => isDesc ? query.OrderByDescending(k => k.Price) : query.OrderBy(k => k.Price),
+                    "seller" => isDesc ? query.OrderByDescending(k => k.Seller) : query.OrderBy(k => k.Seller),
+                    "name" => isDesc ? query.OrderByDescending(k => k.Name) : query.OrderBy(k => k.Name),
+                    _ => query.OrderBy(k => k.Name) 
+                };
+            }
 
             query = sortBy switch
             {
